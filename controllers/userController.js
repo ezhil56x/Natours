31c816c60cdb9d11626ae3d77d0b5908a332c045
Ezhil Shanmugham <ezhil930@gmail.com>
2023-08-08 21:05:41 +0530

    After 7

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet decided!',
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet decided!',
+  });
+};
+
+exports.createuser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet decided!',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet decided!',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet decided!',
+  });
+};
